c113b0d611097bc8df3d2dd59a3806bbc8e52a64
Eduardo Araujo <eduardoaraujo.contato13@gmail.com>
2025-03-08 20:33:21 -0300

    feat: adiciona controle da cobra via teclado

--- js/script.js
+++ js/script.js
@@ -62,3 +62,21 @@ const gameLoop = () => {
 }
 
 gameLoop()
+
+document.addEventListener('keydown', ({key}) => {
+    if (key == "ArrowRight" && direction != "left") {
+        direction = 'right'
+    }
+
+    if (key == "ArrowLeft" && direction != "right") {
+        direction = 'left'
+    }
+
+    if (key == "ArrowDown" && direction != "up") {
+        direction = 'down'
+    }
+
+    if (key == "ArrowUp" && direction != "d own") {
+        direction = 'up'
+    }
+})
